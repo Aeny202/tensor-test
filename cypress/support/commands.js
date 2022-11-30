acfb6665915a6e50049940d383f3ff864c1c1fd3
Dieter <dieter.eyckmans@gmail.com>
2022-11-30 20:22:38 +0100

    ease eslint + eslint fixes

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,44 +1,28 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 import 'cypress-wait-until';
+
+
+/**
+   * A function that changes the value of material sliders.
+   * @name setSliderValue
+   * @kind function
+   * @param {Cypress.Chainable} subject: Cypress element which is the material slider
+   * @param {string} value: value to slide the slider to
+   * @return {void}
+   */
+function setSliderValue({subject, value}) {
+  const nativeInputValueSetter =
+    Object.getOwnPropertyDescriptor(window.HTMLInputElement.prototype, 'value').set;
+  cy.get(subject)
+      .then(($range) => {
+        // get the DOM node
+        const range = $range[0];
+        // set the value manually
+        nativeInputValueSetter.call(range, value);
+        // now dispatch the event
+        range.dispatchEvent(new Event('input', {value: value, bubbles: true}));
+      });
+}
 
 Cypress.Commands.add('setSliderValue', {prevSubject: 'element'}, (subject, value) => {
   setSliderValue({subject: subject, value: value});
-
-  // https://github.com/cypress-io/cypress/issues/1570
-  function setSliderValue({subject, value}) {
-    const nativeInputValueSetter = Object.getOwnPropertyDescriptor(window.HTMLInputElement.prototype, 'value').set;
-    cy.get(subject)
-        .then(($range) => {
-          // get the DOM node
-          const range = $range[0];
-          // set the value manually
-          nativeInputValueSetter.call(range, value);
-          // now dispatch the event
-          range.dispatchEvent(new Event('input', {value: value, bubbles: true}));
-        });
-  }
 });
